0f26c9380e071dc520a3e7660d1eb8d008ffa2d7
Volodymyr Husak <volodyaog2907@gmail.com>
2022-11-09 01:52:09 +0200

    Update Movies.jsx

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,13 +1,16 @@
 export const Movies = () => {
   return (
-    <form className="{css.searchForm}" onSubmit="{handleSubmit}">
+    <form
+      className="{css.searchForm}"
+      // onSubmit="{handleSubmit}"
+    >
       <button className="{css.searchForm_button}" type="submit">
         <span className="{css.searchForm_button_label}">Search</span>
       </button>
 
       <input
         className="{css.searchForm_input}"
-        onChange="{handleImageNameChange}"
+        // onChange="{handleImageNameChange}"
         name="inputImage"
         type="text"
         autoComplete="off"
