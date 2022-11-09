b4a513f96a437387c669348c104b3b7edae0276e
Volodymyr Husak <volodyaog2907@gmail.com>
2022-11-09 23:09:08 +0200

    add img and other information

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,6 +1,8 @@
 import { useParams } from 'react-router-dom';
 // import { loadWithLS } from '../../helpers/storage';
 import { getMovieById } from '../../helpers/getMovieById';
+import { getGenres } from 'helpers/getGenres';
+import css from './MovieDetails.module.css';
 
 export const MovieDetails = () => {
   //   const movies = loadWithLS('PopularMovie');
@@ -8,13 +10,34 @@ export const MovieDetails = () => {
   //   const { id } = useParams();
   const { movieId } = useParams();
   console.log(movieId);
-  const { id, poster_path } = getMovieById(Number(movieId));
+  const { poster_path, original_title, overview, genre_ids } = getMovieById(
+    Number(movieId)
+  );
+  const genres = getGenres(genre_ids);
+  console.log(genre_ids);
   console.log('GET', getMovieById(Number(movieId)));
   //   const product = getProductById(id);
   return (
-    <div>
-      {id}
-      <img src={`https://image.tmdb.org/t/p/w300${poster_path}`} alt="" />
+    <div className={css.movieDetails}>
+      <button className="movieDetails__btn">go back</button>
+      <div className={css.movieDetails__container}>
+        <div>
+          <img
+            className="{css.movieDetails__img}"
+            // width="600"
+            src={`https://image.tmdb.org/t/p/w300${poster_path}`}
+            alt=""
+          />
+        </div>
+        <div>
+          <h2>{original_title}</h2>
+          <p></p>
+          <h3>Overview</h3>
+          <p>{overview}</p>
+          <h4>Genres</h4>
+          <p>{genres}</p>
+        </div>
+      </div>
     </div>
   );
 };
