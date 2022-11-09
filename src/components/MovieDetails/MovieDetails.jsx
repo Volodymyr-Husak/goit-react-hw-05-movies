6cb7528e0948c6c9fd706e3b92947be5aaef21c3
Volodymyr Husak <volodyaog2907@gmail.com>
2022-11-09 23:44:07 +0200

    add year for movie title

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,37 +1,41 @@
 import { useParams } from 'react-router-dom';
-// import { loadWithLS } from '../../helpers/storage';
 import { getMovieById } from '../../helpers/getMovieById';
 import { getGenres } from 'helpers/getGenres';
 import css from './MovieDetails.module.css';
 
 export const MovieDetails = () => {
-  //   const movies = loadWithLS('PopularMovie');
-  //   console.log(movies);
-  //   const { id } = useParams();
   const { movieId } = useParams();
-  console.log(movieId);
-  const { poster_path, original_title, overview, genre_ids } = getMovieById(
-    Number(movieId)
-  );
-  const genres = getGenres(genre_ids);
-  console.log(genre_ids);
+  // console.log(movieId);
+  const {
+    poster_path,
+    original_title,
+    overview,
+    genre_ids,
+    release_date,
+    vote_average,
+  } = getMovieById(Number(movieId));
   console.log('GET', getMovieById(Number(movieId)));
-  //   const product = getProductById(id);
+
+  const genres = getGenres(genre_ids);
+  // console.log(genre_ids);
+
+  const year = release_date.slice(0, 4);
+  const userScore = Math.round(vote_average * 10);
+  // console.log(vote_average * 10);
+  // Math.round(1.2);
+
   return (
     <div className={css.movieDetails}>
       <button className="movieDetails__btn">go back</button>
       <div className={css.movieDetails__container}>
         <div>
-          <img
-            className="{css.movieDetails__img}"
-            // width="600"
-            src={`https://image.tmdb.org/t/p/w300${poster_path}`}
-            alt=""
-          />
+          <img src={`https://image.tmdb.org/t/p/w300${poster_path}`} alt="" />
         </div>
         <div>
-          <h2>{original_title}</h2>
-          <p></p>
+          <h2>
+            {original_title} ({year})
+          </h2>
+          <p>User score {userScore}%</p>
           <h3>Overview</h3>
           <p>{overview}</p>
           <h4>Genres</h4>
