694d47920317acd10164822653a0c49c71f0e15d
Volodymyr Husak <volodyaog2907@gmail.com>
2022-11-10 23:59:13 +0200

    Update Reviews.jsx

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,4 +1,45 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
 export const Reviews = () => {
-  // https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=<<api_key>>&language=en-US
-  return <div>Revieeeeeeews</div>;
+  const { movieId } = useParams();
+  const [reviews, setReviews] = useState([]);
+
+  const API_KEY = '301d018a3b09052968e9ce18b1793bab';
+
+  const fetchReviews = id => {
+    try {
+      fetch(
+        `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${API_KEY}&language=en-US&page=1`
+      )
+        .then(res => res.json())
+        .then(obj => {
+          console.log(obj.results);
+          setReviews(obj.results);
+          return obj.cast;
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchReviews(movieId);
+  }, [movieId]);
+
+  if (reviews.length === 0) {
+    return <p>We don`t have any reviews for this movie</p>;
+  }
+
+  return (
+    <ul>
+      {reviews.map(({ author, id, url, content }) => (
+        <li key={id}>
+          <p>
+            <strong>{author}</strong>
+          </p>
+          <p>{content}</p>
+        </li>
+      ))}
+    </ul>
+  );
 };
